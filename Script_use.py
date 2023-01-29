7dd6f07dc4a78fc4f1dda1272fe21eb9092c94d0
Juan José <114828901+Jeyjei@users.noreply.github.com>
2023-01-29 18:04:16 +0100

    Delete Script_use.py
    Se ha eliminado el archivo de prueba Script_use.py

--- Script_use.py
+++
@@ -1,8 +0,0 @@
-import sys
-import pandas as pd
-import matplotlib.pyplot as plt
-
-print('Number of arguments: %i arguments' % len(sys.argv))
-print('Argument List:' + str(sys.argv))
-
-data = pd.read_csv(sys.argv[1], delimiter = ';')
